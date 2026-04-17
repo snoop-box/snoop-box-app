f4876d9c0d77f531291a74aa9825ee2edaa6c551
DELL <colorunofotografia@gmail.com>
2026-04-17 18:14:26 -0300

    fix railway binding 0.0.0.0

--- server/index.js
+++ server/index.js
@@ -70,6 +70,6 @@ app.get("*", (req, res) => {
 // 🚀 PUERTO
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("🚀 Servidor corriendo en puerto", PORT);
 });
